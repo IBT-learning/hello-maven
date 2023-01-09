c6ac96e5ce84e461495b073141804b765b051955
Daniel Bundor <danielbundor91@gmail.com>
2023-01-09 17:26:38 -0600

    updating App.java file

--- src/main/java/com/ibt/app/App.java
+++ src/main/java/com/ibt/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" this is Daniel Bundor learning Maven; a java build and project management too );
     }
 }
