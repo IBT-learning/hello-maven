22e353643541f2e516340f1879caf5bb1ea5b845
medinat <medinahakinsowon@gmail.com>
2024-06-13 12:50:03 +0100

    updated App.java file

--- src/main/java/com/ibt/app/App.java
+++ src/main/java/com/ibt/app/App.java
@@ -1,13 +1,13 @@
 package com.ibt.app;
 
 /**
- * Hello world!
+ * Hello world - this is Medinat
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World - this is Medinat" );
     }
 }
